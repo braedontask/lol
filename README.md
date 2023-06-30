d40609a9d0e8e687c1779397a13e7f63401bdf43
brae <>
2023-06-30 10:54:06 -0400

    [ init ] push the whole thing

---
+++ README.md
@@ -0,0 +1,15 @@
+# gopherlol
+> [bunnylol](https://www.quora.com/What-is-Facebooks-bunnylol) / [bunny1](http://www.bunny1.org) -like smart bookmarking tool, written in Go
+
+```bash
+go run . 
+# then add `http://localhost:8080/?q=%s` as a search engine to your browser 
+```
+
+## How to set as a browser search engine
+- in Chrome: [Set your default search engine](https://support.google.com/chrome/answer/95426)
+- Instructions for all major browsers: [How-To Geek: How to Add Any Search Engine to Your Web Browser](https://www.howtogeek.com/114176/how-to-easily-create-search-plugins-add-any-search-engine-to-your-browser/)
+
+## FAQ
+- What are the commands currently supported by gopherlol? => Query gopherlol for `help` or `list`, or see [commands/commands.go](commands/commands.go)
+- Why would a company run such a service internally? => Read about [how Facebook uses it internally](http://www.ccheever.com/blog/?p=74)
---
+++ commands/commands.go
@@ -0,0 +1,93 @@
+package commands
+
+import (
+	"fmt"
+	"net/url"
+	"os"
+	"time"
+)
+
+type Commands struct{}
+
+func (c *Commands) Help() {
+	// We want this method to show up in `help`/`list` results
+	// But the actual logic for these commands is in `main.go`
+}
+
+func (c *Commands) List() {
+	// We want this method to show up in `help`/`list` results
+	// But the actual logic for these commands is in `main.go`
+}
+
+func (c *Commands) G(cmdArg string) string {
+	return fmt.Sprintf("https://www.google.com/search?q=%s", url.QueryEscape(cmdArg))
+}
+
+func (c *Commands) Egs(cmdArg string) string {
+	return fmt.Sprintf("https://github.com/search?q=repo%%3Aget-wrecked%%2Fmedal-electron+%s&type=code", url.QueryEscape(cmdArg))
+}
+
+func (c *Commands) Cgs(cmdArg string) string {
+	return fmt.Sprintf("https://github.com/search?q=repo%%3Aget-wrecked%%2Fmedal_components+%s&type=code", url.QueryEscape(cmdArg))
+}
+
+func (c *Commands) Bgs(cmdArg string) string {
+	return fmt.Sprintf("https://github.com/search?q=repo%%3Aget-wrecked%%2Fmedal-backend+%s&type=code", url.QueryEscape(cmdArg))
+}
+
+func (c *Commands) Cal(cmdArg string) string {
+	return "https://calendar.google.com/calendar/u/1/r"
+}
+
+func (c *Commands) Pcal(cmdArg string) string {
+	return "https://calendar.google.com/calendar/u/0/r"
+}
+
+func (c *Commands) Mail(cmdArg string) string {
+	return "https://mail.google.com/mail/u/1/#inbox"
+}
+
+func (c *Commands) Pmail(cmdArg string) string {
+	return "https://mail.google.com/mail/u/0/#inbox"
+}
+
+func (c *Commands) Gcp(cmdArg string) string {
+	return "https://console.cloud.google.com/sql/instances/clipz/users?authuser=1&project=medal-2995f"
+}
+
+func (c *Commands) X(cmdArg string) string {
+	return "https://linear.app/medal/project/extermination-befd7a005236/US1"
+}
+
+func (c *Commands) Chat(cmdArg string) string {
+	return "https://chat.openai.com/"
+}
+
+func (c *Commands) Paste(cmdArg string) string {
+	currentTime := time.Now()
+	fileName := currentTime.Format("20060102150405") + ".txt"
+	filePath := "./pastes/" + fileName
+
+	file, err := os.OpenFile(filePath, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0644)
+	if err != nil {
+		// TODO: write an error page
+		return "https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley"
+	}
+	defer file.Close()
+
+	_, err = file.WriteString(cmdArg)
+	if err != nil {
+		// TODO: write an error page
+		return "https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley"
+	}
+
+	err = file.Sync()
+	if err != nil {
+		return "https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley"
+	}
+
+	return "https://www.youtube.com/watch?v=QTmkibc3WcI&ab_channel=Fai_rong"
+
+	// TODO: figure out why tf this breaks??
+	// return "file:///Users/braedon/Documents/gopherlol/pastes/" + fileName
+}
